e52333a91acaf0310a4ffd3787b77bd08747e507
Marcin Murawski <mrc.murawski@gmail.com>
2018-05-26 15:21:41 +0200

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "groups/index"
+
+  resources :groups
+
+  root "groups#show"
 end
