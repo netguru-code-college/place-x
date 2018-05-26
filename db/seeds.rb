c71e2f146c125a5881619ac196d357c857e42719
Tomasz Skupiński <sqpa@Tomaszs-MacBook-Pro.local>
2018-05-26 13:08:31 +0200

    [#1] Added Place scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!([
+  {
+    name: 'Random Place in Gdansk',
+    description: "Just a random place in Gdańsk",
+    lat: 54.380292,
+    lng: 18.611262
+  },
+  {
+    name: '2nd Random Place in Gdansk',
+    description: "Just another random place in Gdańsk",
+    lat: 52.380292,
+    lng: 20.611262
+  }
+])
